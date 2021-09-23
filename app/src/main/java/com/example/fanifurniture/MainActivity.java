f09ca730f10653ad015cdab97e07882539854780
damvulong <kingdamlong1708@gmail.com>
2021-09-24 01:02:36 +0700

    add bottom navigation

--- app/src/main/java/com/example/fanifurniture/MainActivity.java
+++ app/src/main/java/com/example/fanifurniture/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.fanifurniture;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private Fragment selectorFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        selectorFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_search:
+                        selectorFragment = new SearchFragment();
+                        break;
+                    case R.id.nav_cart:
+                        selectorFragment = new CartFragment();
+                        break;
+                    case R.id.nav_profile:
+                        selectorFragment = new ProfileFragment();
+                        break;
+                }
+                if (selectorFragment != null){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectorFragment).commit();
+                }
+                return true;
+            }
+        });
     }
 }
